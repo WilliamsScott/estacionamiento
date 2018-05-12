c90c7f1e35e40bc8158a18921921dc21c8da2236
llaupi <37598344+llaupi@users.noreply.github.com>
2018-05-12 20:05:06 -0300

    holaa

---
+++ application/views/principal.php
@@ -0,0 +1,59 @@
+<body class="cyan">
+	<div class="row">
+		<div class="col s12 m4 offset-m4">
+			<div class="card">
+				<div class="ca
+				rd-action teal lighten-3 center-align white-text">
+					<h3>iniciar sesion</h3>
+				</div>
+				<div class="card-content">
+
+					<div class="row">
+						<form class="col s12">
+							<div class="row">
+								<div class="input-field">
+									<i class="material-icons prefix">account_circle</i>
+									<input id="nombre" type="text" class="validate">
+									<label for="nombre">cliente</label>
+								</div>
+							</div>
+							<div class="row">
+								<div class="input-field ">
+									<i class="material-icons prefix">lock</i>
+									<input id="clave" type="text" class="validate">
+									<label for="clave">clave</label>
+								</div>
+							</div>
+							<div class="row">
+								<button class="btn waves-effect waves-light right" type="submit" name="action">ingresar
+									<a href="<?php echo base_url()?>Welcome/adminindex">fof</a>
+									<i class="material-icons right">send</i>
+								</button>
+							</div>
+
+
+						</form>
+
+					</div>
+				</div>
+
+			</div>
+
+		</div>
+	</div>
+	</div>
+	</div>
+
+	</div>
+
+
+
+
+
+
+
+
+
+
+
+	</body
---
+++ application/views/templates/footer.php
@@ -0,0 +1,5 @@
+   <!--JavaScript at end of body for optimized loading-->
+      <script type="text/javascript" src=" <?php echo base_url()?>assets/js/materialize.min.js"></script>
+        <script type="text/javascript" src=" <?php echo base_url()?>assets/js/custom.js"></script>
+    </body>
+  </html>
