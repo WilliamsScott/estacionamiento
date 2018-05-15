a4a11447a96953bdbc16952ee7bf2b9aaa4d311c
llaupi <37598344+llaupi@users.noreply.github.com>
2018-05-15 19:36:31 -0400

    jsjs

--- application/views/administrador/inicio.php
+++ application/views/administrador/inicio.php
@@ -2,33 +2,48 @@
 
 
 	<header></header>
-<main>
-<nav class="nav-extended blue-grey darken-4">
-    <div class="nav-wrapper">
-      <a href="#" class="brand-logo"><i class="material-icons">cloud</i></a>
-     
-      <ul id="nav-mobile" class="right hide-on-med-and-down ">
-        <li><a href="sass.html">inicio</a></li>
-        <li><a href="badges.html">perfil</a></li>
-        <li><a href="collapsible.html">Salir</a></li>
-      </ul>
-    </div>
-    <div class="nav-content ">
-      <ul class="tabs tabs-transparent light-blue darken-4 ">
-        <li class="tab active"><a href="#test1">Estacionamiento</a></li>
-        <li class="tab"><a class="" href="#test2">Camaras</a></li>    
-        <li class="tab"><a href="#test3">registro</a></li>
-      </ul>
-    </div>
-  </nav>
-  <div id="test1" class="col s12">autosasdasdasdaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa <br>dadasdaa <br>sadasdasdad <br>asdasda</div>
-  <div id="test2" class="col s12">grabaciones</div>
-  <div id="test3" class="col s12">registros</div>
+	<main>
+		<nav class="nav-extended blue-grey darken-4 ">
+			<div class="nav-wrapper">
+				<a href="#" class="brand-logo">
+					<i class="material-icons">cloud</i>
+				</a>
 
-</main>
+				<ul id="nav-mobile" class="right hide-on-med-and-down ">
+					<li>
+						<a href="sass.html">inicio</a>
+					</li>
+					<li>
+						<a href="badges.html">perfil</a>
+					</li>
+					<li>
+						<a href="collapsible.html">Salir</a>
+					</li>
+				</ul>
+			</div>
+			<div class="nav-content s12">
+				<ul class="tabs tabs-transparent light-blue darken-4 ">
+					<li class="tab active col s4">
+						<a href="#test1">͏͏͏Estacionamiento</a>
+					</li>
+					<li class="tab col s4">
+						<a class="" href="#test2">Camaras</a>
+					</li>
+					<li class="tab col s4">
+						<a href="#test3">registro</a>
+					</li>
+					<li class="tab col s4">
+						<a href="#test4">personal</a>
+					</li>
+				</ul>
+			</div>
+		</nav>
 
+		<div id="test1" class="col s12">
+			<p>aqui iran todas las opciones del estacionamiento en donde haran muchas cosas interesantes</p>
+		</div>
+		<div id="test2" class="col s12">Comming soon</div>
+		<div id="test3" class="col s12">registros</div>
+		<div id="test4" class="col s12">aqui deberian haber personas</div>
 
-
- 
-        
-
+	</main>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -8,14 +8,17 @@
       <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       <title>inicio</title>
-      <style>body{
+   <style>
+    body {
     display: flex;
-    min-height: 200vh;
+    min-height: 100vh;
     flex-direction: column;
   }
+
   main {
     flex: 1 0 auto;
-  }</style>
+  }
+      </style>
       <!--Import materialize.css-->
       <link type="text/css" rel="stylesheet" href="<?php echo base_url()?>assets/css/materialize.min.css"  media="screen,projection"/>
      
